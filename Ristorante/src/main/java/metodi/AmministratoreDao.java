2efcb9ccea016bbc1e8f90517f58c746c9f60b77
Mario_Di_Biase <mariodibiase1993@gmail.com>
2023-09-20 15:01:21 +0200

    Aggiunti i Dao

--- Ristorante/src/main/java/metodi/AmministratoreDao.java
+++ Ristorante/src/main/java/metodi/AmministratoreDao.java
@@ -1,12 +1,34 @@
 package metodi;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import connessione.Connessione;
 import model.Amministratore;
 
 public class AmministratoreDao {
 	
+	private Connection con = null;
+	
 	public void inserire (Amministratore amm) {
+		
+		String query = "INSERT INTO amministratore( id, nome, cognome, username, password) values(?,?,?,?,?)";
+
+		try {
+			con = Connessione.getInstance().getConnection();
+			PreparedStatement pst = con.prepareStatement(query);
+
+			pst.setInt(1, id);
+			pst.setString(2, nome);
+			pst.setString(2, nome);
+			pst.executeUpdate();
+
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+
 		
 	}
 	public void modifica(Amministratore amm) {
