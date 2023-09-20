9ed5c94350bc68071c27045a4a51e6aee0c45794
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-09-20 15:16:06 +0200

    eliminato

--- Ristorante/src/main/java/metodi/Test.java
+++ Ristorante/src/main/java/metodi/Test.java
@@ -5,17 +5,7 @@ import model.Amministratore;
 public class Test {
 	
 	public static void main(String[] args) {
-		AmministratoreDao amm = new AmministratoreDao();
-		Amministratore amminis = new Amministratore();
-		amminis.setId_ammi(1);
-		amminis.setNome("Javier");
-		amminis.setCognome("acosta");
-		amminis.setUsername("chico230");
-		amminis.setPassword("1234");
 		
-		amm.inserire(amminis);
-		//amm.modifica(amminis);
-		amm.lista().stream().forEach(li-> System.out.println(li.getNome()));
 		
 	}
 
