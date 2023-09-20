23548aaa61ed42ef9ea8aade084edde2c11414ef
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-09-20 12:18:36 +0200

    aggiunto la connessione

---
+++ Ristorante/src/main/java/connessione/Connessione.java
@@ -0,0 +1,36 @@
+package connessione;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Connessione {
+	private static Connessione instance;
+
+	public static Connessione getInstance() {
+		if (instance == null) {
+			instance = new Connessione();
+		}
+		return instance;
+	}
+
+	private Connection conn;
+
+	private Connessione() {
+		Properties connectionProps = new Properties();
+		connectionProps.put("user", "root");
+		connectionProps.put("password", "root");
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ristorante", connectionProps);
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() throws SQLException, ClassNotFoundException {
+		return conn;
+	}
+}
