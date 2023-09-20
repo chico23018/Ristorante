8f78ae7d32d5554dad25cc589d6eda6a51d8a05b
Mario_Di_Biase <mariodibiase1993@gmail.com>
2023-09-20 15:01:54 +0200

    Aggiunti i Dao

---
+++ Ristorante/src/main/java/metodi/OrdineDao.java
@@ -0,0 +1,22 @@
+package metodi;
+
+import java.util.List;
+
+import model.Ordine;
+
+public class OrdineDao {
+
+public void inserire (Ordine ord) {
+		
+	}
+	public void modifica(Ordine ord) {
+		
+	}
+	public void elimina(int id_ord) {
+		
+	}
+	public List<Ordine> dl(){
+		return null;
+	}
+	
+}
---
+++ Ristorante/src/main/java/metodi/TavoloDao.java
@@ -0,0 +1,22 @@
+package metodi;
+
+import java.util.List;
+
+import model.Tavolo;
+
+public class TavoloDao {
+
+	
+public void inserire (Tavolo tavolo) {
+		
+	}
+	public void modifica(Tavolo tavolo) {
+		
+	}
+	public void elimina(int id_tavolo) {
+		
+	}
+	public List<Tavolo> dl(){
+		return null;
+	}
+}
