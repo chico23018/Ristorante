f71f0ba0da1d197c885b1fac5e3664345a9bfca4
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-09-21 14:23:26 +0200

    modificato home

--- Ristorante/src/main/java/control/Login.java
+++ Ristorante/src/main/java/control/Login.java
@@ -9,32 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Login
- */
+
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
@@ -42,7 +27,7 @@ public class Login extends HttpServlet {
 		String password =request.getParameter("password");
 		
 		System.out.println( nome+ " "+ password);
-		RequestDispatcher rd= request.getRequestDispatcher("./view/home.jsp");
+		RequestDispatcher rd= request.getRequestDispatcher("home.jsp");
 		rd.forward(request, response);
 		doGet(request, response);
 	}
